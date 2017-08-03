e61c72d5f5db83138a39dfbbe9dbfd7245cfe8a1
Edvards Umbrass <e.umbrass@weblab.lv>
2017-08-03 09:51:00 +0300

    composer update and index.php file changes

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jkuprijanovs
- * Date: 7/12/2017
- * Time: 2:15 PM
- */
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$client = new WebLabLv\Renoks\Client\PriceClient(
-    'https://renoks-car-parts-gateway-jkuprijanovs.weblab.lv/api/v0.1',
-    'test',
-    'test'
-);
-
+$client = new WebLabLv\Renoks\Client\PriceClient('renoks-gateway-eumbrass.weblab.lv/api/v0.1', 'client1', 'clientpass');
 $client->sendRequest();
-echo substr($client->getResponseJson(), 0, 100);
+
+echo PHP_EOL;
+echo substr($client->getResponseJson(), 0, 150);
 echo PHP_EOL;
 echo PHP_EOL;
 print_r(current($client->getResponseArray()));
